825b397867345fe8258484ac37ca133eb53137b8
Maxim Stanko <maxim.stanko@student.hpi.uni-potsdam.de>
2025-03-16 16:33:42 +0100

    finished title (actually)

---
+++ scripts/changeSceneButtonControl.gd
@@ -0,0 +1,9 @@
+extends TextureButton
+
+@export var scenePath: String
+
+func _ready():
+	pressed.connect(Callable(self, "change_scene"))
+
+func change_scene():
+	get_tree().change_scene_to_file(scenePath)
