573f6b73e220dbca3336a774c0258c1bd0f0e41c
Maxim Stanko <maxim.stanko@student.hpi.uni-potsdam.de>
2025-03-16 17:36:41 +0100

    short slider and stuff

--- scripts/area_2d.gd
+++ scripts/area_2d.gd
@@ -5,10 +5,13 @@ signal collected
 @onready var animationPlayer = get_child(0)
 @onready var particles = get_child(1) 
 
+@export var appearingButton: TextureButton
+
 var hasEntered = false
 
 func _on_body_entered(body):
 	if body.is_in_group("player") and not hasEntered:
 		particles.set_emitting(false)
-		animationPlayer.play("disappear")
 		hasEntered = true
+		animationPlayer.play("disappear")
+		appearingButton.appear()
