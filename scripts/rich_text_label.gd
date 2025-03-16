ea135bc6cc8c3375c2d327ab6473517c2b1c3b81
Maxim Stanko <maxim.stanko@student.hpi.uni-potsdam.de>
2025-03-16 02:00:40 +0100

    finished title screen

--- scripts/rich_text_label.gd
+++ scripts/rich_text_label.gd
@@ -1,30 +1,24 @@
 extends RichTextLabel
 
-var message = "This is a text, which will be told by a narrator.\nThere is absolutely nothing you need to worry about, it's just gonna work."
-var cascading_delay = 0.05
-var char_timers
+var char_timers: Array = []
 
 func _ready():
 	bbcode_enabled = true
-	text = message
-	fade_text(message, cascading_delay)
 
-func fade_text(string, cascading_delay):
+func fade_text(string, cascading_delay = 0.05):
 	char_timers = []
 	var i = 0
-	
 	for char in string:
 		char_timers.append ({
 			letter   = char,
 			delay    = cascading_delay * i,
 		})
-		
 		i += 1
 	
 func _process(delta):
 	text = ""
 	for c_timer in char_timers:
-		var color   = Color(1,1,1,1)
+		var color = Color(1,1,1,1)
 		
 		if(c_timer.delay > 0.0):
 			c_timer.delay -= delta
